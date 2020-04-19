4a6fa88009e22d5d660a9df316a11d4cf78d2f27
clementbruno <clmnt.bruno@gmail.com>
2020-04-19 16:55:12 +0200

    add new routes in our shop

--- src/pages/collection/collection.jsx
+++ src/pages/collection/collection.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 import CollectionItem from "../../components/collection-item/collection-item.component";
 import { connect } from "react-redux";
-import { createStructuredSelector } from "reselect";
 import { selectCollection } from "../../redux/shop/shop-selectors";
 import "./collection.scss";
 
-const CollectionPage = ({ match, collection }) => {
+const CollectionPage = ({ collection }) => {
+  const { title, items } = collection;
+
   return (
     <div className="collection-page">
-      <h2>CollectionPage</h2>
-      <CollectionItem collection={collection} />
+      <h2 className="title">{title}</h2>
+      <div className="items">
+        {items.map((item) => {
+          return <CollectionItem key={item.id} item={item} />;
+        })}
+      </div>
     </div>
   );
 };
 
-const mapStateToProps = createStructuredSelector({
-  collection: selectCollection,
-});
+const mapStateToProps = (state, ownProps) => {
+  return {
+    collection: selectCollection(ownProps.match.params.collectionId)(state),
+  };
+};
 
 export default connect(mapStateToProps)(CollectionPage);
